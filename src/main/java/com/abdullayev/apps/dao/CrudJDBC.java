b113ae45499a138b8f748fb751b0a6413fffbcd3
Ramazan <ramazan.abdullayev@bsu.edu.az>
2025-03-05 16:43:48 +0400

    JPA_CRUD_APP

---
+++ src/main/java/com/abdullayev/apps/dao/CrudJDBC.java
@@ -0,0 +1,127 @@
+package com.abdullayev.apps.dao;
+
+import com.abdullayev.apps.entity.Student;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CrudJDBC {
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/test_db";
+    private static final String DB_USER = "jpauser";
+    private static final String DB_PASSWORD = "jpapwd";
+
+
+    public List<Student> index() {
+        List<Student> students = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER,DB_PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(
+                     "SELECT * FROM test_db.students")) {
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while(rs.next()) {
+                    Student student = new Student();
+                    student.setId(rs.getLong("id"));
+                    student.setName(rs.getString("name"));
+                    student.setSurname(rs.getString("surname"));
+                    student.setAvgGrade(rs.getDouble("avg_grade"));
+
+                    students.add(student);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return students;
+    }
+
+    public Student findById(Long id) {
+        Student student = null;
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(
+                     "SELECT * FROM test_db.students WHERE id=?")) {
+
+            ps.setLong(1, id);
+
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
+                    student = new Student();
+                    student.setId(resultSet.getLong("id"));
+                    student.setName(resultSet.getString("name"));
+                    student.setSurname(resultSet.getString("surname"));
+                    student.setAvgGrade(resultSet.getDouble("avg_grade"));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return student;
+    }
+
+    public void insertToBD(Student student) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(
+                     "INSERT INTO test_db.students (name, surname, avg_grade) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+
+            ps.setString(1, student.getName());
+            ps.setString(2, student.getSurname());
+            ps.setDouble(3, student.getAvgGrade());
+
+            int affectedRows = ps.executeUpdate();
+            if(affectedRows == 0) {
+                throw new SQLException("Failed to add Student to DB");
+            }
+
+            // Синхронизация между добавляемым объектом и строкой в таблице (БД)
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                student.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("Failed to create Student ID");
+            }
+            generatedKeys.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(Student updateStudent, Long id) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(
+                     "UPDATE test_db.students SET name=?, surname=?, avg_grade=? WHERE id=?")) {
+
+            ps.setString(1, updateStudent.getName());
+            ps.setString(2, updateStudent.getSurname());
+            ps.setDouble(3, updateStudent.getAvgGrade());
+            ps.setLong(4, id);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void delete(Long id) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(
+                     "DELETE FROM test_db.students WHERE id=?")) {
+
+            ps.setLong(1, id);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/abdullayev/apps/dao/CrudJPA.java
@@ -0,0 +1,85 @@
+package com.abdullayev.apps.dao;
+
+import com.abdullayev.apps.entity.Student;
+import jakarta.persistence.*;
+
+
+public class CrudJPA {
+    EntityManagerFactory factory = Persistence
+            .createEntityManagerFactory("jpa-learn");
+    EntityManager entityManager = factory.createEntityManager();
+    EntityTransaction transaction = entityManager.getTransaction();
+
+
+    public void insertToBD(Student student) {
+        try {
+            transaction.begin();
+            entityManager.persist(student);
+            transaction.commit(); // Коммитит в БД новый объект
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Student findByID(int id) {
+        Student student = null;
+        try {
+            student = entityManager.find(Student.class, id);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return student;
+    }
+
+    public void update(String name, String surname, Double avgGrade, int id) {
+        Student student = null;
+        try {
+            transaction.begin();
+            student = entityManager.find(Student.class, id);
+            if (name != null) {
+                student.setName(name);
+            }
+            if (surname != null) {
+                student.setSurname(surname);
+            }
+            if (avgGrade != null) {
+                student.setAvgGrade(avgGrade);
+            }
+            transaction.commit(); // Обеспечивает синхронизацию между объектом java приложения и строкой таблицы
+                                  // Изменил объект - изменится и соответсвующая строка в таблице - и наоборот
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void remove(int id) {
+        try {
+            transaction.begin();
+            Student student = entityManager.find(Student.class, id);
+            entityManager.remove(student);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void closeResources() {
+        if (entityManager != null) {
+            entityManager.close();
+        }
+        if (factory != null) {
+            factory.close();
+        }
+    }
+
+}
